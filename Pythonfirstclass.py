7c3e6d88578ce841d5cb7f74c6cf9570e62f896d
Sean Lynch <lynchtsean@gmail.com>
2025-06-16 20:11:48 -0400

    thirdrun

--- Pythonfirstclass.py
+++ Pythonfirstclass.py
@@ -1,2 +1,9 @@
 print ("Hello, world!")
 print (1,2,3,4,5,6,7,8,9,10)
+
+a = int(input("Enter first number:" \
+b = int(input("Enter second number:"))
+print(*Sum is:",a + b)
+      
+num = int(input("Enter second number:")) if num % 2 == 0:
+print("Even") else: print("Odd")
